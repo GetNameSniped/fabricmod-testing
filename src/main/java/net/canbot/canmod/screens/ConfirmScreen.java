28bea2cb3e5ba1c41c2e4a25f0c6c74b9813257e
CannedWither <74307590+GetNameSniped@users.noreply.github.com>
2023-08-17 22:03:04 +0200

    Also forgot to update fabric.mod.json

--- src/main/java/net/canbot/canmod/screens/ConfirmScreen.java
+++ src/main/java/net/canbot/canmod/screens/ConfirmScreen.java
@@ -12,7 +12,9 @@ import java.util.List;
 import java.util.Objects;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.font.MultilineText;
+import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.util.math.MatrixStack;
@@ -70,12 +72,12 @@ public class ConfirmScreen extends Screen {
         this.buttons.add((ButtonWidget)this.addDrawableChild(button));
     }
 
-    public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
-        this.renderBackground(matrices);
+    public void render(DrawContext context, int mouseX, int mouseY, float delta) {
+        this.renderBackground(context);
 
-        drawCenteredTextWithShadow(matrices, this.textRenderer, this.title, this.width / 2, this.getTitleY(), 16777215);
-        this.messageSplit.drawCenterWithShadow(matrices, this.width / 2, this.getMessageY());
-        super.render(matrices, mouseX, mouseY, delta);
+        context.drawCenteredTextWithShadow(this.textRenderer, this.title, this.width / 2, this.getTitleY(), 16777215);
+        this.messageSplit.drawCenterWithShadow(context, this.width / 2, this.getMessageY());
+        super.render(context, mouseX, mouseY, delta);
     }
 
     private int getTitleY() {
